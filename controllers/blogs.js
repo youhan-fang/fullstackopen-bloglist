5db50f1f8cd019d80b8143c73cb6810f81483c02
Youhan Fang <yfang@purdue.edu>
2021-09-21 15:14:26 -0700

    exercise 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,17 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 require('express-async-errors');
 
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
@@ -10,12 +19,19 @@ blogsRouter.get('/', async (request, response) => {
 });
 
 blogsRouter.post('/', async (request, response) => {
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({
+      error: 'token missing or invalid'
+    });
+  }
   if (!request.body.title || !request.body.url) {
     return response.status(400).json({
       error: 'required information is missing'
     });
   }
-  const user = await User.findOne({});
+  const user = await User.findById(decodedToken.id);
   const blog = new Blog({
     title: request.body.title,
     author: request.body.author,
