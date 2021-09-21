5763a35fed302e71072234fca8f8d082b9655c8d
Youhan Fang <yfang@purdue.edu>
2021-09-21 12:11:55 -0700

    exercise 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,10 +1,11 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 require('express-async-errors');
 
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
 
@@ -14,13 +15,17 @@ blogsRouter.post('/', async (request, response) => {
       error: 'required information is missing'
     });
   }
+  const user = await User.findOne({});
   const blog = new Blog({
     title: request.body.title,
     author: request.body.author,
     url: request.body.url,
-    likes: request.body.likes || 0
+    likes: request.body.likes || 0,
+    user: user._id
   });
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
   response.status(201).json(savedBlog);
 });
 
--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1 });
   response.status(200).json(users);
 });
 
